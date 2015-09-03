f29a9ecc218b3849567e0f3eafa46a7e5aa2c692
Lance <Lance@thelpstory.com>
2015-09-03 16:15:11 -0600

    adding quiz/results objects

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,8 +1,28 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
+router.get('/user', function(req, res, next) {
   res.render('index', { title: 'Node-Translate' });
 });
+
+router.post('/user', function(req, res, next) {
+  res.render('index', { title: 'Node-Translate' });
+});
+
+router.put('/user', function(req, res, next) {
+  res.render('index', { title: 'Node-Translate' });
+});
+
+// word library
+router.get('/wordlibrary', function(req, res, next) {
+  res.render('index', { title: 'Node-Translate' });
+});
+
+router.post('/wordlibrary', function(req, res, next) {
+  res.render('index', { title: 'Node-Translate' });
+});
+
+
+
 
 module.exports = router;
